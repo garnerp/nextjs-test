d000d90442c2c14f65eeebe0d6983f65aed4c8fc
Jeff Escalante <gh.je@mailhero.io>
2019-02-07 16:45:56 -0500

    use hashicorp plugin

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
 const withCSS = require('@zeit/next-css')
 const withTypescript = require('@zeit/next-typescript')
-const path = require('path')
 const getClient = require('./lib/apolloClient')
 const withGraphql = require('next-plugin-graphql')
 const gql = require('graphql-tag')
+const withHashicorp = require('@hashicorp/next-hashicorp')
 
 const config = {
   async exportPathMap() {
@@ -32,19 +32,6 @@ const config = {
       analyzerMode: 'static',
       reportFilename: '../bundles/client.html'
     }
-  },
-  // setup for markdown file loading
-  webpack: config => {
-    config.module.rules.push({
-      test: /\.mdx?$/,
-      use: [
-        'babel-loader',
-        '@mdx-js/loader',
-        path.join(__dirname, 'lib/mdx-layout-loader')
-      ]
-    })
-    config.resolve.symlinks = false
-    return config
   }
 }
 
@@ -67,5 +54,5 @@ function loadFromDato() {
 }
 
 module.exports = withTypescript(
-  withGraphql(withCSS(withBundleAnalyzer(config)))
+  withGraphql(withCSS(withBundleAnalyzer(withHashicorp(config))))
 )
